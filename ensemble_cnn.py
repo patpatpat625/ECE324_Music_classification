0a027b1fbe40fafeb7c06676aeb07eafd06065c5
Xitong(Jacqueline) Zhang <60076577+xtzhang1122@users.noreply.github.com>
2022-04-14 15:33:57 -0400

    final code for CNN Ensemble

--- ensemble_cnn.py
+++ ensemble_cnn.py
@@ -6,49 +6,10 @@ import matplotlib.pyplot as plt
 
 # model definition
 # Using CNNs
-class CNN_Ensemble1(nn.Module):
+class CNN_Ensemble(nn.Module):
     def __init__(self):
         # call super to initialize the class above in the hierarchy
-        super(CNN_Ensemble1, self).__init__()
-        # spectrogram CNN
-        self.network1 = torch.nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=3, kernel_size=3, padding='same'),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, padding='same'),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(in_channels=16, out_channels=1, kernel_size=3, padding='same'),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2))
-
-        # feature CNN
-        self.network2 = torch.nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=3, kernel_size=3, padding='same'),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, padding='same'),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(in_channels=16, out_channels=1, kernel_size=3, padding='same'),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2))
-        # combined
-        self.linear = nn.Linear(3392, 8)
-
-    def forward(self, x1, x2):
-        one = self.network1(x1)
-        two = self.network2(x2)
-
-        # flatten and concat the two matrices
-        x = torch.cat((torch.flatten(one, start_dim=1), torch.flatten(two, start_dim=1)), dim=1)
-        return torch.sigmoid(self.linear(x))
-
-
-class CNN_Ensemble2(nn.Module):
-    def __init__(self):
-        # call super to initialize the class above in the hierarchy
-        super(CNN_Ensemble2, self).__init__()
+        super(CNN_Ensemble, self).__init__()
 
         # spectrogram CNN
         self.network3 = torch.nn.Sequential(
@@ -56,81 +17,40 @@ class CNN_Ensemble2(nn.Module):
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
             nn.BatchNorm2d(num_features=3),
-            nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, padding='same'),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.BatchNorm2d(num_features=16),
-            nn.Conv2d(in_channels=16, out_channels=1, kernel_size=3, padding='same'),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.BatchNorm2d(num_features=1))
 
-        # feature CNN
-        self.network4 = torch.nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=3, kernel_size=3, padding='same'),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.BatchNorm2d(num_features=3),
-            nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, padding='same'),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.BatchNorm2d(num_features=16),
-            nn.Conv2d(in_channels=16, out_channels=1, kernel_size=3, padding='same'),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.BatchNorm2d(num_features=1))
-
-        # combined
-        self.linear = nn.Linear(3392, 8)
-
-    def forward(self, x1, x2):
-
-        three = self.network3(x1)
-        four = self.network4(x2)
-
-        # flatten and concat the two matrices
-        x = torch.cat((torch.flatten(three, start_dim=1), torch.flatten(four, start_dim=1)), dim=1)
-        return torch.sigmoid(self.linear(x))
-
-
-class CNN_Ensemble3(nn.Module):
-    def __init__(self):
-        # call super to initialize the class above in the hierarchy
-        super(CNN_Ensemble3, self).__init__()
-
-        # spectrogram CNN
-        self.network3 = torch.nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=3, kernel_size=3, padding='same'),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.BatchNorm2d(num_features=3),
             nn.Conv2d(in_channels=3, out_channels=3, kernel_size=3, padding='same'),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
             nn.BatchNorm2d(num_features=3))
 
         # feature CNN
-        self.network4 = torch.nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=3, kernel_size=3, padding='same'),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.BatchNorm2d(num_features=3),
-            nn.Conv2d(in_channels=3, out_channels=6, kernel_size=3, padding='same'),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.BatchNorm2d(num_features=6),
-            nn.Conv2d(in_channels=6, out_channels=3, kernel_size=3, padding='same'),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.BatchNorm2d(num_features=3),
-            nn.Conv2d(in_channels=3, out_channels=1, kernel_size=3, padding='same'),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.BatchNorm2d(num_features=1))
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=3, kernel_size=3, padding='same')
+        self.conv2 = nn.Conv2d(in_channels=3, out_channels=8, kernel_size=3, padding='same')
+        self.conv3 = nn.Conv2d(in_channels=8, out_channels=3, kernel_size=3, padding='same')
 
+        self.conv1.weight.data.uniform_(0, 0.05)
+        self.conv2.weight.data.uniform_(0, 0.05)
+        self.conv3.weight.data.uniform_(0, 0.05)
+
+        self.network4 = torch.nn.Sequential(
+            self.conv1,
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.BatchNorm2d(num_features=3),
+
+            self.conv2,
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.BatchNorm2d(num_features=8),
+
+            self.conv3,
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.BatchNorm2d(num_features=3))
+        
         # combined
-        #self.linear = nn.Linear(5496, 8)
-        self.linear = nn.Linear(10896, 8)
+        self.linear1 = nn.Linear(17904, 8)
+
 
     def forward(self, x1, x2):
 
@@ -139,7 +59,7 @@ class CNN_Ensemble3(nn.Module):
 
         # flatten and concat the two matrices
         x = torch.cat((torch.flatten(three, start_dim=1), torch.flatten(four, start_dim=1)), dim=1)
-        return torch.sigmoid(self.linear(x))
+        return torch.sigmoid(self.linear1(x))
 
 
 def plot(title, y):
@@ -154,43 +74,42 @@ def plot(title, y):
 
 if __name__ == "__main__":
     device = 'cpu'
-    np.random.seed(555)
     # load data and convert to tensor
-    dir = "D:\\music_classifier\\data_10\\"
+    dir = "C:\\Users\\xtzha\\Desktop\\ECE324\\ECE324_Music_classification\\data\\"
 
-    x1_train = np.load(dir+"train\\spectrogram.npy")
+    x1_train = np.load(dir+"train10\\spectrogram.npy")
     x1_train = torch.from_numpy(x1_train).unsqueeze(1).float().to(device)
-    x2_train = np.load(dir + "train\\feature.npy")
+    x2_train = np.load(dir + "train10\\feature.npy")
     x2_train = torch.from_numpy(x2_train).unsqueeze(1).float().to(device)
-    y_train = np.load(dir+"train\\label.npy")
+    y_train = np.load(dir+"train10\\label.npy")
     y_train = torch.tensor(y_train).type(torch.LongTensor).to(device)
 
-    x1_val = np.load(dir + "val\\spectrogram.npy")
+    x1_val = np.load(dir + "val10\\spectrogram.npy")
     x1_val = torch.from_numpy(x1_val).unsqueeze(1).float().to(device)
-    x2_val = np.load(dir + "val\\feature.npy")
+    x2_val = np.load(dir + "val10\\feature.npy")
     x2_val = torch.from_numpy(x2_val).unsqueeze(1).float().to(device)
-    y_val = np.load(dir + "val\\label.npy")
+    y_val = np.load(dir + "val10\\label.npy")
     y_val = torch.tensor(y_val).type(torch.LongTensor).to(device)
 
-    x1_test = np.load(dir + "test\\spectrogram.npy")
+    x1_test = np.load(dir + "test10\\spectrogram.npy")
     x1_test = torch.from_numpy(x1_test).unsqueeze(1).float().to(device)
-    x2_test = np.load(dir + "test\\feature.npy")
+    x2_test = np.load(dir + "test10\\feature.npy")
     x2_test = torch.from_numpy(x2_test).unsqueeze(1).float().to(device)
-    y_test = np.load(dir + "test\\label.npy")
+    y_test = np.load(dir + "test10\\label.npy")
     y_test = torch.tensor(y_test).type(torch.LongTensor).to(device)
 
     # define parameters
     epochs = 20
     lr = 0.0001
-    batch = 150
+    batch = 128
 
     # Initialize model
-    model = CNN_Ensemble3().to(device)
+    model = CNN_Ensemble().to(device)
 
     # Initialize loss function and optimizer
     loss = nn.CrossEntropyLoss()
-    #optimizer = torch.optim.SGD(model.parameters(), lr, momentum = 0.9)
     optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=1e-6)
+
     # index array
     indices = np.arange(x1_train.shape[0])
 
